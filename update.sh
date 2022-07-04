fbdf6e2528b546e87a6b3433d573499581228949
Robotizing <>
2022-07-04 15:44:11 +0300

    Work outside the repository directory

--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@
 
 INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-bin="$INSTALL_DIR/miceweb"
-binpaths="/usr/local/bin"
-
 OLDVER="$(miceweb version)"
+cd "$INSTALL_DIR"
 git pull
-if [ $? -ne 0 ]; then
+RET=$?
+cd -
+if [ $RET -ne 0 ]; then
 	exit 1
 fi
 NEWVER="$(miceweb version)"
