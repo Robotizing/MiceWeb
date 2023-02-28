87ecf0d5f9adebfbb2f2eae03133f4e3fcf42f0b
Robotizing <>
2023-02-28 13:36:38 +0200

    Optimized update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@
 INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 miceweb backup >/dev/null
-OLDVER="$(miceweb version 2>/dev/null)"
+#OLDVER="$(miceweb version 2>/dev/null)"
 cd "$INSTALL_DIR"
 git pull
 RET=$?
@@ -29,9 +29,8 @@ if [ $RET -ne 0 ]; then
 	exit 1
 fi
 "$INSTALL_DIR/install.sh"
-NEWVER="$(miceweb version 2>/dev/null)"
-if [ "$OLDVER" != "$NEWVER" ]; then
-	echo "Updated from v.$OLDVER to v.$NEWVER"
-	echo "" 1>&2
-fi
-
+#NEWVER="$(miceweb version 2>/dev/null)"
+#if [ "$OLDVER" != "$NEWVER" ]; then
+#	echo "Updated from v.$OLDVER to v.$NEWVER"
+#	echo "" 1>&2
+#fi
